649adc2e03c1019f7a2b6f4322f6745bdd88c151
programmersumanhalder <programmersumanhalder@gmail.com>
2021-04-07 10:48:05 +0300

    u

---
+++ README.md
@@ -0,0 +1,10 @@
+# SNAKE GAME
+
+# Technologies used:
+## Html,css, vanilla js
+
+# Instructions :
+1) first you need to press key(any arrow key=down,up,left,right) to start the game 
+2)Once you start game in up position and then you can not move in down position and same for left and right direction.You can play only one direction for one time but you can move vertically when you start to move horizontally .
+# Little Description:
+This game made to sharp my modern vanilla js skills.
---
+++ game.js
@@ -0,0 +1,40 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED,getSnakeHead,snakeIntersection } from "./snake.js";
+
+import {update as updateFood, draw as drawFood} from './food.js'
+import { outsideGrid } from "./grid.js";
+let lastRenderTime=0;
+let gameOver= false
+const gameBoard=document.getElementById('game-board')
+function main(currentTime){
+  if(gameOver){
+    if(confirm('you lost.press ok to restart.')){
+      window.location= '/'
+    }
+    return
+  }
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender=(currentTime - lastRenderTime)/1000
+if(secondsSinceLastRender < 1 /SNAKE_SPEED) return
+
+lastRenderTime=currentTime
+
+update()
+draw()
+checkDeath()
+
+}
+window.requestAnimationFrame(main)
+
+function update(){
+updateSnake()
+updateFood()
+}
+function draw(){
+  gameBoard.innerHTML=''
+drawSnake(gameBoard)
+drawFood(gameBoard)
+}
+
+function checkDeath(){
+  gameOver=outsideGrid(getSnakeHead()) || snakeIntersection()
+}
